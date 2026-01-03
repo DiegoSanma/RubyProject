996e0cd20dbc9fe269a9bbe72abaf9f97094c572
DiegoSanma <diego@DiegoSanma.>
2026-01-03 17:01:51 -0300

    Pleaaaaseee

---
+++ frontend/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  let data = null;
+  let error = null;
+
+  async function loadBackend() {
+    try {
+      const res = await fetch('/api/v1/health');
+      data = await res.json();
+    } catch (e) {
+      error = e;
+    }
+  }
+
+  loadBackend();
+</script>
+
+<main>
+  <h1>Svelte + Rails API :)</h1>
+
+  {#if data}
+    <pre>{JSON.stringify(data, null, 2)}</pre>
+  {/if}
+
+  {#if error}
+    <p style="color:red">{error.message}</p>
+  {/if}
+</main>
