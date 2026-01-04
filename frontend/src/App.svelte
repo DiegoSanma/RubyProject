4bce0ee0b9d7fb1e02362ad4fbbfdee7c5dab344
benjaestupinan <benja.3stupinan@gmail.com>
2026-01-04 15:29:12 -0300

    homepage, sybau

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,13 @@
 <script>
   import { Router, Route } from 'svelte-routing'
   import Login from './routes/Login.svelte'
+  import Homepage from './routes/Homepage.svelte'
 </script>
 <Router>
   <Route path="/login">
     <Login />
+  </Route>
+  <Route path="/">
+    <Homepage />
   </Route>
 </Router>
